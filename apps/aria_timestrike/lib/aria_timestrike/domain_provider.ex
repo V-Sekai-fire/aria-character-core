71d79a2245dac20bf5d5a01bfaaed21c91d46ac8
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-06-15 00:05:33 -0700

    Fix AriaTimestrike domain provider module naming and function references
    Updated AriaTimestrike.Core.DomainProvider to AriaTimestrike.DomainProvider
    and corrected the create_domain function call to use AriaTimestrike.create_domain()
    instead of AriaTimestrike.Core.create_domain(). This resolves the undefined
    function warnings and aligns with the app renaming from aria_timestrike_core
    to aria_timestrike.

--- apps/aria_timestrike/lib/aria_timestrike/domain_provider.ex
+++ apps/aria_timestrike/lib/aria_timestrike/domain_provider.ex
@@ -1,18 +1,18 @@
 # Copyright (c) 2025-present K. S. Ernest (iFire) Lee
 # SPDX-License-Identifier: MIT
 
-defmodule AriaTimestrike.Core.DomainProvider do
+defmodule AriaTimestrike.DomainProvider do
   @moduledoc """
-  Domain provider for TimeStrike core (non-temporal) game domain functionality.
+  Domain provider for TimeStrike temporal game domain functionality.
   """
 
   @behaviour AriaEngine.DomainProvider
 
   @impl true
-  def domain_type, do: "timestrike_core"
+  def domain_type, do: "timestrike"
 
   @impl true
   def create_domain do
-    AriaTimestrike.Core.create_domain()
+    AriaTimestrike.create_domain()
   end
 end
