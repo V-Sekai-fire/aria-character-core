cdf53bde55f2b04aec08d66bab0b93f73ce40dec
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-07-02 13:58:24 -0700

    Remove unused PlannerBehaviour and PlannerAdapter modules to streamline the codebase

--- apps/aria_hybrid_planner/lib/planning/core_interface.ex
+++ apps/aria_hybrid_planner/lib/planning/core_interface.ex
@@ -9,15 +9,6 @@ defmodule Planning.CoreInterface do
   @type solution_tree :: Core.solution_tree()
   @type plan_step :: Core.plan_step()
   @type todo_item :: Core.todo_item()
-  @doc "Simple planning interface - finds a plan to achieve the given todos.\n"
-  @spec plan(AriaEngine.DomainBehaviour.t(), AriaEngine.Core.state(), [todo_item()], keyword()) ::
-          {:ok, solution_tree()} | {:error, String.t()}
-  def plan(_domain, _state, _todos, _opts \\ []) do
-    # Note: AriaEngine.PlannerAdapter.plan currently only returns {:error, String.t()}
-    # This is a stub implementation - full planning requires aria_hybrid_planner integration
-    # TODO: Implement actual planning logic using aria_hybrid_planner
-    {:error, "Planning not yet implemented in self-contained mode"}
-  end
 
   @doc "Executes a plan step by step, returning the final state.\n"
   @spec execute_plan(AriaEngine.DomainBehaviour.t(), AriaEngine.Core.state(), [plan_step()]) ::
@@ -25,25 +16,6 @@ defmodule Planning.CoreInterface do
   def execute_plan(_domain, _initial_state, _plan) do
     # TODO: Implement actual plan execution using aria_hybrid_planner
     {:error, "Plan execution not yet implemented in self-contained mode"}
-  end
-
-  @doc "Replan from a failure point using HybridPlanner.HybridCoordinator.\n"
-  @spec replan(Core.t(), String.t(), keyword()) ::
-          {:ok, Core.t()} | {:error, String.t()}
-  def replan(engine, fail_node_id, opts \\ [])
-
-  def replan(%Core{solution_tree: solution_tree} = engine, _fail_node_id, _opts)
-      when not is_nil(solution_tree) do
-    _domain_interface = Internal.to_planner_interface(engine)
-
-    # Note: AriaEngine.PlannerAdapter.replan currently only returns {:error, String.t()}
-    # This is a stub implementation - full replanning requires aria_hybrid_planner integration
-    # TODO: Implement actual replanning logic using aria_hybrid_planner
-    {:error, "Replanning not yet implemented in self-contained mode"}
-  end
-
-  def replan(%Core{solution_tree: nil}, _fail_node_id, _opts) do
-    {:error, "No solution tree available for replanning"}
   end
 
   @doc "Validate the current plan.\n"
